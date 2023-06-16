50af3abe02326756f5bbea3b65de79d753290b21
Duwan Fortunato <duwancontacto@gmail.com>
2023-06-16 11:23:18 -0400

    Add bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import "../languages/i18n";
 import {Provider} from "react-redux";
 import {store} from "@/store";
 
+import "bootstrap/dist/css/bootstrap.css";
+
 export default function App({Component, pageProps}: AppProps) {
   return (
     <Provider store={store}>
