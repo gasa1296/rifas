e29a8790c63b98e56eb1f6feb853fa7a27c2280e
Duwan <duwancontacto@gmail.com>
2023-08-10 10:44:01 -0400

    pequenos cambios

--- src/components/Rifas/AllRifas.tsx
+++ src/components/Rifas/AllRifas.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AiFillCamera, AiFillDelete, AiFillCloseCircle } from "react-icons/ai";
-import fondoAllRifas from "@/assets/img/fondoAllRifas.svg"
+import fondoAllRifas from "@/assets/img/gal-microsite-apac1.jpg"
 import Image from "next/image";
 import Rifa from './Rifa';
 import RifasActivas from './RifasActivas';
@@ -9,16 +9,16 @@ import RifasActivas from './RifasActivas';
 export default function AllRifas() {
     return (
         <div className=''>
-            <div className='d-block d-lg-flex   justify-content-around '>
-                <div className=' '>
-                    <p className=' fw-semibold display-3 opacity-50 mt-5 mt-lg-4 '>¡Rífate! </p>
-                    <p className='fw-normal display-6 opacity-75 my-4'>Súmate con amor</p>
+            <div className='d-block d-lg-flex   justify-content-center   '>
+                <div className=' col-12 col-lg-5 '>
+                    <p className=' title-participating mt-5 mt-lg-4 '>¡Rífate! </p>
+                    <p className='fw-normal display-6 opacity-75 my-4'> Y súmate con amor</p>
                     <p className='fw-semibold fs-5 opacity-75 m-0'>Aquí encontraras las historias con las Que te puedes rifar las</p>
-                    <p className='fw-semibold fs-5 opacity-75 m-0'>  Que te puedes rifar</p>
+                    <p className='fw-semibold fs-5 opacity-75 m-0 '>  Que te puedes rifar</p>
 
                 </div>
-                <div className='mx-5 mt-2'>
-                    <Image src={fondoAllRifas} alt='fondoAllrifas' className='w-100' />
+                <div className='mx-5 mt-2 col-12 col-lg-5 '>
+                    <Image src={fondoAllRifas} alt='fondoAllrifas' className='w-100 h-100 ' />
                 </div>
             </div>
             <Rifa />
