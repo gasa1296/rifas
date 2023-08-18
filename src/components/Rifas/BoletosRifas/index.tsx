334f5ee22775821516ba02cbe107c16bb68354b8
Duwan Fortunato <duwanp2112@gmail.com>
2023-08-18 14:22:07 -0400

    Rifas

---
+++ src/components/Rifas/BoletosRifas/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { AiOutlineArrowRight } from "react-icons/ai";
+
+import BuyTickes from "../BuyTickes";
+import Tickets from "./Tickets";
+import TicketReview from "./TicketReview";
+import { useSelector } from "react-redux";
+import { selectRaffleState } from "@/store/slices/raffles";
+
+export default function BoletosRifas({ nextStep }: any) {
+  const handleSubmit = () => {
+    nextStep();
+  };
+
+  return (
+    <div className="d-block justify-content-center d-lg-flex  mt-5  ">
+      <div className=" col-12 col-lg-6  ">
+        <BuyTickes />
+      </div>
+
+      <div className=" col-lg-6 col-12 ms-4  ">
+        <p className="donaciones-title mt-3 mt-md-0">
+          1 / 3 Selecciona tus boletos
+        </p>
+        <p className="subtitle-boletosaRifa ">
+          Da clic para seleccionar tus boletos
+        </p>
+        <Tickets />
+        <TicketReview />
+
+        <div className="d-flex justify-content-end mt-4 ">
+          <button
+            className="btn btn-pink btn-sm border rounded-1 px-3 "
+            onClick={handleSubmit}
+          >
+            {" "}
+            Continuar <AiOutlineArrowRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
